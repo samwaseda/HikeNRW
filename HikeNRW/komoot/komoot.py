7f5ef6d65d4ed69a973f85c1d4ea20e7233b927c
Sam Waseda <waseda@mpie.de>
2026-04-09 12:37:58 +0200

    Put all information in komoot.py

--- HikeNRW/komoot/komoot.py
+++ HikeNRW/komoot/komoot.py
@@ -1,6 +1,26 @@
-from kompy import KomootConnector
 import os
+import re
+from kompy import KomootConnector
 from datetime import timedelta
+
+
+def extract_komoot_id(text):
+    # Define the regular expression pattern for 9 to 11 digit numbers
+    pattern = r"\b\d{9,11}\b"
+    # Find all matches in the text
+    matches = re.findall(pattern, text)
+    if len(matches) == 0:
+        raise ValueError("There is no Komoot ID in the string")
+    elif len(matches) > 1:
+        raise ValueError("Multiple Komoot ID got detected")
+    return matches[0]
+
+
+def extract_komoot_url(text):
+    matches = re.findall(r"https?://(?:www\.)?komoot[^\s]*", text)
+    if len(matches) == 0:
+        raise ValueError("No komoot link found")
+    return matches[0]
 
 
 def get_html(komoot_id):
@@ -9,7 +29,8 @@ def get_html(komoot_id):
         return f.readline().replace("TOURID", str(komoot_id))
 
 
-def get_komoot_dict(komoot_id):
+def get_komoot_dict(komoot_message):
+    komoot_id = extract_komoot_id(komoot_message)
     connector = KomootConnector(
         password=os.environ["komoot_psw"],
         email=os.environ["komoot_email"],
@@ -29,6 +50,7 @@ def get_komoot_dict(komoot_id):
         "url": f"https://www.komoot.com/de-de/tour/{tour.id}",
         "html": get_html(tour.id),
         "tour": tour,
+        "url": extract_komoot_url(komoot_message),
         "vector_image": tour.vector_map_image.image,
         "links_dict": tour.links_dict,
     }
